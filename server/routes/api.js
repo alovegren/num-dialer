77db638ed1c8e3af53b3f3f048589484a61827ba
Missy Lovegren <audrey.lovegren@gmail.com>
2022-06-30 14:01:34 -0500

    Update imports
    Co-authored-by: Andrew Moore <andrewdmoore84@gmail.com>

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import callsController from '../controllers/callsService.js';
+import callsController from '../controllers/callsController.js';
 
 router.post('/calls', callsController.initCalls);
 router.post('/callStatuses', callsController.updateCallStatus);
