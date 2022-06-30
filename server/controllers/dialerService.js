e858e2df32f4b15de32f560acaeab829c95a9a1f
Missy Lovegren <audrey.lovegren@gmail.com>
2022-06-30 12:45:26 -0500

    Begin implementing webhook endpoint
    Co-authored-by: Andrew Moore <andrewdmoore84@gmail.com>

--- server/controllers/dialerService.js
+++ server/controllers/dialerService.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 
 const DIALER_BASE_URL = 'http://localhost:4830';
+const WEBHOOK_URL = 'http://localhost:3000/api/callStatuses';
+const PHONE_NUMBER = '13018040009';
 /*
 1. hits external API
 2. handle webhooks coming in from external API
 */
 
 const service = {
-  dial: (phone, webhook) => {
+  dial: () => {
     console.log('dialing!');
-    // axios.post(`${DIALER_BASE_URL}/call`, { phone, webhook });
+    axios.post(`${DIALER_BASE_URL}/call`, { phone: PHONE_NUMBER, webhookURL: WEBHOOK_URL });
   },
+
+  updateCallStatus: ({ body }) => {
+    console.log(body);
+  }
 }
 
 export default service;
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import dialerService from '../controllers/dialerService.js';
 
 router.post('/calls', dialerService.dial);
+router.post('/callStatuses', dialerService.updateCallStatus);
 
 export default router;
